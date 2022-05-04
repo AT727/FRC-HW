58f34235b5aa76d2b96613159216ee83b690e075
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-04 15:20:38 -0400

    Create SqrtN.java
    This is very bad program that crashes computer if it's not a perfect square

---
+++ SqrtN.java
@@ -0,0 +1,18 @@
+class sqrtN{
+  
+  public void sqrtOfN(double n){
+    double loop = 1;
+    while(true){
+    double answer = n/loop;
+    if (answer == loop){
+      System.out.println("Square root of " + n + " is " + answer);
+      break;
+    }
+    else{
+      System.out.println(answer + " " + loop);
+    }
+      
+    loop += 1;
+  }
+}
+}
