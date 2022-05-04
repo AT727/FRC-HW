19c507063331bd19395b5c8b221349533e288ad5
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-04 15:18:29 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    QuadraticSolver obj1= new QuadraticSolver();
+    FizzBuzz obj2 = new FizzBuzz();
+    Factorial obj3 = new Factorial();
+    obj3.findFactorial(5);
+    
+  }
+}
